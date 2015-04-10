6e0fc80e5ed854c4e2108028c9347f986fa50ebd
James Skemp <strivinglife@gmail.com>
2015-04-10 18:34:52 -0500

    Standard updates for widths and heights, title, and Android needs.

--- desktop/src/com/jamesrskemp/libgdx/tiledmapdemo/desktop/DesktopLauncher.java
+++ desktop/src/com/jamesrskemp/libgdx/tiledmapdemo/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.jamesrskemp.libgdx.tiledmapdemo.TiledMapDemo;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		config.title = "libGDX TiledMap Demo";
+
 		new LwjglApplication(new TiledMapDemo(), config);
 	}
 }
