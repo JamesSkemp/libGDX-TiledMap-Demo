e35ab7e1337e33a6c019898313d84c47631f5b9d
James Skemp <strivinglife@gmail.com>
2015-04-10 18:57:08 -0500

    Desktop application can now generate packed assets.

--- desktop/src/com/jamesrskemp/libgdx/tiledmapdemo/desktop/DesktopLauncher.java
+++ desktop/src/com/jamesrskemp/libgdx/tiledmapdemo/desktop/DesktopLauncher.java
@@ -2,10 +2,19 @@ package com.jamesrskemp.libgdx.tiledmapdemo.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.jamesrskemp.libgdx.tiledmapdemo.TiledMapDemo;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		boolean packAssets = false;
+		if (packAssets) {
+			TexturePacker.Settings settings = new TexturePacker.Settings();
+			settings.useIndexes = false;
+			TexturePacker.process(settings, "raw", "packed", "sample.atlas");
+		}
+
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 480;
